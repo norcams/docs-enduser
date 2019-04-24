8423ad6f71c54dbab55104f4eedd52abf195cc8a
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2019-04-24 14:01:58 +0200

    terraform

---
+++ docs/downloads/multiple.tf
@@ -0,0 +1,15 @@
+provider "openstack" {}
+
+resource "openstack_compute_instance_v2" "basic" {
+  count = 5
+  name = "test-${count.index}"
+  image_name = "GOLD CentOS 7"
+  flavor_name = "m1.small"
+
+  key_pair = "mykey"
+  security_groups = [ "default", "SSH and ICMP" ]
+
+  network {
+    name = "IPv6"
+  }
+}
