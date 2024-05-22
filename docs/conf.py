73f2ac04e5da96ba4504680675f947805230ee7f
Trond Hasle Amundsen <trondham@uio.no>
2024-05-22 13:32:26 +0200

    try _static again

--- docs/conf.py
+++ docs/conf.py
@@ -146,7 +146,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 # Path should be relative to the ``_static`` files directory.
-html_logo = "logo.png"
+html_logo = "_static/logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
